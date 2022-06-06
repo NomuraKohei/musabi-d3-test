30305483eb67cc2b3b7375f404338ba3a03e9a4d
NomuraKohei <seiryu2nd@gmail.com>
2022-06-06 20:17:34 +0900

    fix: 微調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,12 +37,12 @@ export default function Home() {
                     name='description'
                     content='2002年から2020年までの月毎の海水温の平均をビジュアライズした。' />
                 <link rel='icon' href='/favicon.ico' />
-                <meta property="og:url" content="https://musabi-d3-test.vercel.app/" />
+                <meta property="og:url" content="https://seawater-temperature-data-visualization.vercel.app" />
                 <meta property="og:title" content="Seawater TemperatureData Visualization" />
                 <meta property="og:site_name" content="Seawater TemperatureData Visualization" />
                 <meta property="og:description" content="月ごとの海水温の平均をビジュアライズ" />
                 <meta property="og:type" content="website" />
-                <meta property="og:image" content="ogpImage" />
+                <meta property="og:image" content={ogpImage.src} />
                 <meta property="og:image:width" content={String(1260)} />
                 <meta property="og:image:height" content={String(600)} />
             </Head>
@@ -52,13 +52,13 @@ export default function Home() {
                     {!isScaleDown && <React.Fragment>
                         <h1 className={styles.title}>Seawater Temperature <br />Data Visualization</h1>
                         <p className={styles.description}>月ごとの海水温の平均をビジュアライズ</p>
-                        <p className={styles.cite}>データ取得元：<cite><a href="https://jdoss1.jodc.go.jp/vpage/coastal_j.html" rel="noopener noreferrer">海洋データセンター</a></cite></p>
+                        <p className={styles.cite}>データ取得元：<cite className={styles.citeText}><a href="https://jdoss1.jodc.go.jp/vpage/coastal_j.html" rel="noopener noreferrer">海洋データセンター</a></cite></p>
                     </React.Fragment>}
-                    <button onClick={onClickHandler} className={styles.scaleButton}>{isScaleDown ? '戻す' : '画面全体に表示'}</button>
+                    <button onClick={onClickHandler} className={styles.scaleButton}>{isScaleDown ? '戻す' : '画面全体に表示を切替'}</button>
                 </section>
                 <MyD3Component isScaleDown={isScaleDown} />
             </main>
-            <footer className={styles.footer}><small className={styles.small}>© Nomura All right received</small></footer>
+            <footer className={styles.footer}><small className={styles.small}>© Nomura All right reserved</small></footer>
         </div>
     );
 }
